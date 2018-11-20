4331b250ab1ae641c218ebd7239a44757c7a92ff
manjor <manoeltavares.jr@gmail.com>
2018-11-20 17:19:29 -0300

    Operadores: atribuição

---
+++ fundamentos/operadores/atribuicao.js
@@ -0,0 +1,19 @@
+//Atribuição Normal
+const a = 7
+let b = 3
+
+//Atribuição aditiva ou b = b + a
+b += a // b = b + a
+console.log(b)
+
+b -= 4 // b = b - 4
+console.log(b)
+
+b *= 2 // b = b * 2
+console.log(b)
+
+b/= 2 // b = b / 2
+console.log(b)
+
+b %= 2 // b = b % 2
+console.log(b)
