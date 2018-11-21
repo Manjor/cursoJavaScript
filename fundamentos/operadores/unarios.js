54ab75c7262da77516ddc39b203bd8919ba52fe2
manjor <manoeltavares.jr@gmail.com>
2018-11-21 17:17:17 -0300

    Operadores: Unarios

---
+++ fundamentos/operadores/unarios.js
@@ -0,0 +1,15 @@
+let num1 = 1
+let num2 = 2
+
+//Pos fixida
+//Acrescenta uma unidade ao valor da variavel
+num1++
+console.log(num1)
+//Pré Fixida
+//Remove uma unidade ao valor da variavel
+--num1
+console.log(num1)
+
+
+console.log(++num1 === num2--)
+console.log(num1 === num2)
