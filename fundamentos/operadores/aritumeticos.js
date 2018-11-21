9ae64e09fe01ddf7e7e814e851f501a77f9553e5
Manoel <manoeltavares.jr@gmail.com>
2018-11-21 10:41:56 -0300

    Opradores: Aritméticos

---
+++ fundamentos/operadores/aritumeticos.js
@@ -0,0 +1,15 @@
+const [a,b,c,d] = [3,5,1,15]
+
+const soma = a + b + c + d
+console.log("Soma: " ,soma)
+
+const sub = d - c
+console.log('Subtracao: ' ,sub)
+
+const mult = d * c
+console.log('Multiplicacao: ' ,mult)
+const div = d / c
+console.log('Divisao: ' ,div)
+
+const modulo = d % c
+console.log('Modulo: ' ,modulo)
