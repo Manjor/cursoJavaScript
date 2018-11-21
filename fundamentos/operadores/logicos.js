ea7a99d34715fc9f7bb6ba283e122e713ca2c372
manjor <manoeltavares.jr@gmail.com>
2018-11-21 17:09:01 -0300

    Operadores: Logicos

---
+++ fundamentos/operadores/logicos.js
@@ -0,0 +1,21 @@
+function compras(trabalho1, trabalho2){
+    
+    const comprarSorvete = trabalho1 || trabalho2
+    const comprarTv50 = trabalho1 && trabalho2
+    //const comprarTv32 = !!(trabalho1 ^ trabalho2) //bitwise xor
+    const comprarTv32 = trabalho1 != trabalho2
+    const manterSaudavel = !comprarSorvete //operador unitario
+
+    return { 
+            //Cria um objeto com par de chave e valor.
+            comprarSorvete,
+            comprarTv50,
+            comprarTv32,
+            manterSaudavel}
+    }
+
+    console.log(compras(true,true))
+    console.log(compras(true,false))
+    console.log(compras(false,true))
+    console.log(compras(false,false))
+
