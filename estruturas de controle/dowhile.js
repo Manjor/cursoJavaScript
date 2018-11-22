e8d27e07625a2bed5a751675115883aae327273d
Manoel <manoeltavares.jr@gmail.com>
2018-11-22 10:59:27 -0300

    Estruturas: DO WHILE

---
+++ estruturas de controle/dowhile.js
@@ -0,0 +1,12 @@
+function getInteiroAleatorio(min,max){
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+
+let opcao = 0
+do{
+    opcao = getInteiroAleatorio(-1,10)
+    console.log(`Opção escolhida foi ${opcao}`)
+    
+}while(opcao != -1)
