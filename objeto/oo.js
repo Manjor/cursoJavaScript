5989fc614e5f29e0524d1f6d53cf8920fc96054a
manjor <manoeltavares.jr@gmail.com>
2018-11-26 15:31:04 -0300

    Objeto: Introdução

---
+++ objeto/oo.js
@@ -0,0 +1,20 @@
+//Código não executável
+
+//Procedural - Baseado no procedimento, ou seja, em funções
+processamento(valor1,valor2,valor3)
+
+//OO - Orientação a Objetos - Dados que possuem funções
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        // ...
+    }
+}
+objeto.processamento() //Foco passou a ser o Objeto
+ //Principios Importantens:
+ //1-Abstração
+ //2-Encapsulamento
+ //3-Herança (prototype)
+ //4-polimorfismo
