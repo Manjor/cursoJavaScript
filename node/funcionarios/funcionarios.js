c8915f3b7c8884daedb66c9d02e2f60609b9a8be
Manoel Tavares <36523015+Manjor@users.noreply.github.com>
2018-12-06 17:39:53 -0300

    Node: Dependencias e Scripts

--- node/funcionarios/funcionarios.js
+++ node/funcionarios/funcionarios.js
@@ -9,7 +9,7 @@ const menorSalario = (func,funcAtual) =>{
 
 axios.get(url).then(response =>{
     const funcionarios = response.data
-    console.log(funcionarios)
+    //console.log(funcionarios)
 
     const func = funcionarios
         .filter(chineses)
