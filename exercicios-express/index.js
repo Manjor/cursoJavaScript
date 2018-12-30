d5d0a9b4c1e4abf3eb997ef450234e7259e50682
Manoel Tavares <36523015+Manjor@users.noreply.github.com>
2018-12-30 13:29:47 -0300

    Express: Primeira Requisição

--- exercicios-express/index.js
+++ exercicios-express/index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 
+//Use atende todos os tipos de requisições, seja ela, GET, POST, PUT...
+/*app.use('/opa',(req,res)=>{
+    res.send('<h1>Estou Bem</h1>')
+})*/
+app.all('/opa',(req,res)=>{
+    res.send('<h1>Requisição</h1>')
+})
+
 app.listen(3000,()=>{
     console.log('Backend Executando...')
 })
