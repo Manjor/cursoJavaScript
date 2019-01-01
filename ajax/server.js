1ad4d3d2c62ad553fa2023794c35fd01a5c1a0b6
Manoel Tavares <36523015+Manjor@users.noreply.github.com>
2019-01-01 11:48:31 -0300

    Ajax: Axios #2

--- ajax/server.js
+++ ajax/server.js
@@ -35,7 +35,15 @@ app.post('/formulario',(req,res)=>{
 
 app.get('/teste',(req,res)=>{ res.send('Ok')})
 
-app.get('',(req,res)=>{})
+app.get('/parOuImpar',(req,res)=>{
+    //req.body
+    //req.query
+    //req.params
+    const par = parseInt(req.query.numero) % 2 === 0
+    res.send({
+        resultado: par ? 'par':'impar'
+    })
+})
 app.get('',(req,res)=>{})
 app.get('',(req,res)=>{})
 
