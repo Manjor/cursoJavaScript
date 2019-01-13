cb72636fc6e3764154f4d8426836504969094b09
Manoel Tavares <36523015+Manjor@users.noreply.github.com>
2019-01-13 15:54:03 -0300

    Projeto Final: Validar Cadastro de Administradores

--- projeto_Knowledge/backend/api/user.js
+++ projeto_Knowledge/backend/api/user.js
@@ -15,6 +15,9 @@ module.exports = app => {
         const user = {...req.body}
         if(req.params.id) user.id = req.params.id
 
+        if(!req.originalUrl.startsWith('/users')) user.admin = false
+        if(!req.user || !req.user.admin) user.admin = false
+
         try{
             existsOrError(user.name,'Nome não Informado.')
             existsOrError(user.email,'Email não Informado.')
