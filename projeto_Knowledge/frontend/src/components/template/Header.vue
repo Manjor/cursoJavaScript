9c1a0ffa8dfb47f2e3ea3542aa0f742faf915a2a
manjor <manoeltavares.jr@gmail.com>
2019-01-16 09:22:39 -0300

    Projeto Final: Componete Header

--- projeto_Knowledge/frontend/src/components/template/Header.vue
+++ projeto_Knowledge/frontend/src/components/template/Header.vue
@@ -1,10 +1,32 @@
 <template>
-    <header class="header"></header>
+    <header class="header">
+        <a class="toggle" @click="toggleMenu" v-if="!hideToggle">
+            <i class="fa fa-lg" :class="icon"></i>
+        </a>
+        <h1 class="title">
+            {{title}}
+        </h1>
+    </header>
 </template>
 
 <script>
 export default {
-    name: 'Header'
+    name: 'Header',
+    //Propriedades do Component
+    props:{
+        title: String,
+        hideToggle: Boolean
+    },
+    computed:{
+        icon(){
+            return "fa-angle-left"
+        }
+    },
+    methods:{
+        toggleMenu(){
+            
+        }
+    }
 }
 </script>
 
@@ -16,5 +38,33 @@ export default {
         display: flex;
         justify-content: center;
         align-items: center;
+    }
+    .title{
+        font-size: 1.2rem;
+        color: white;
+        font-weight: lighter;
+        flex-grow: 1;
+        text-align: center;
+    }
+    .title a{
+        color: #FFF;
+        text-decoration: none;
+    }
+
+    header.header > a.toggle {
+        width: 60px;
+        height: 100%;
+        color: #FFF;
+        justify-self: flex-start;
+        text-decoration: none;
+        
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    header.header > a.toggle:hover{
+        background-color: #222a;
+        transition: 0.6s;
     }
 </style>
