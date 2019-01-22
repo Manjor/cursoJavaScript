bc3024b13ba9113b70dcce3cd4566f338445aae2
manjor <manoeltavares.jr@gmail.com>
2019-01-22 10:06:49 -0300

    Projeto Final: Cadastro Usuário #1

--- projeto_Knowledge/frontend/src/main.js
+++ projeto_Knowledge/frontend/src/main.js
@@ -10,7 +10,7 @@ import router from './config/router'
 Vue.config.productionTip = false
 
 //Temporário
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Ik1hbm9lbCIsImVtYWlsIjoibWFub2VsdGF2YXJlc0BlbWFpbC5jb20iLCJhZG1pbiI6dHJ1ZSwiYWl0IjoxNTQ4MTU3NDY0LCJleHAiOjE1NDg0MTY2NjR9.xs-cSMDwx69LQ5q1KU6Vmp5f9L1P6-SKP9BnQ-BrRfg'
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Ik1hbm9lbCIsImVtYWlsIjoibWFub2VsdGF2YXJlc0BlbWFpbC5jb20iLCJhZG1pbiI6dHJ1ZSwiYWl0IjoxNTQ4MTYwMjM1LCJleHAiOjE1NDg0MTk0MzV9.uB08gSv8itWB7PzUfA6DKw1zFpr2sBStMdg992nJiAY'
 
 new Vue({
   store,
