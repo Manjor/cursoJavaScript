daab6cc4b38de4ecbc49ca6c405f2ff63f0b5a68
manjor <manoeltavares.jr@gmail.com>
2019-01-22 09:23:38 -0300

    Projeto FInal: Páginas Administrativas

--- projeto_Knowledge/frontend/src/components/admin/AdminPages.vue
+++ projeto_Knowledge/frontend/src/components/admin/AdminPages.vue
@@ -1,14 +1,33 @@
 <template>
     <div class="admin-page">
         <PageTitle icon="fa fa-cogs" main="Administração do Sistema" sub="Cadastros & Cia" />
+        <div class="admin-pages-tabs">
+            <b-card no-body>
+                <b-tabs card>
+                    <b-tab title="Artigos" active>
+                        <ArticleAdmin/>
+                    </b-tab>
+                    <b-tab title="Categorias">
+                        <CategoryAdmin/>
+                    </b-tab>
+                    <b-tab title="Usuários">
+                        <UserAdmin/>
+                    </b-tab>
+                </b-tabs>
+            </b-card>
+        </div>
     </div>
 </template>
 
 <script>
 import PageTitle from '../template/PageTitle'
+import ArticleAdmin from './ArticleAdmin'
+import CategoryAdmin from './CategoryAdmin'
+import UserAdmin from './UserAdmin'
+
 export default {
     name: 'AdminPage',
-    components: { PageTitle }
+    components: { PageTitle,ArticleAdmin,CategoryAdmin,UserAdmin }
 }
 </script>
 
