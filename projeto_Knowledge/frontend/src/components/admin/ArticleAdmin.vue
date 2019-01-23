8d540db860a921b1b81aa054cc1a77adebe38393
manjor <manoeltavares.jr@gmail.com>
2019-01-23 16:02:20 -0300

    Projeto Final: Cadastro Artigo #2

--- projeto_Knowledge/frontend/src/components/admin/ArticleAdmin.vue
+++ projeto_Knowledge/frontend/src/components/admin/ArticleAdmin.vue
@@ -53,6 +53,7 @@
                 </b-button>
             </template>
         </b-table>
+        <b-pagination size="md" v-model="page" :total-rows="count" :per-page="limit"/>
     </div>
 </template>
 
@@ -104,7 +105,7 @@ export default {
 
         },
         loadArticles(){
-            const url = `${baseApiUrl}/articles`
+            const url = `${baseApiUrl}/articles?page=${this.page}`
             axios.get(url).then( res =>{
                 //this.articles = res.data
                 this.articles = res.data.data
@@ -139,6 +140,11 @@ export default {
                     return {value:user.id, text: `${user.name} - ${user.email}`}
                 })
             })
+        }
+    },
+    watch:{
+        page(){
+            this.loadArticles()
         }
     },
     mounted(){
