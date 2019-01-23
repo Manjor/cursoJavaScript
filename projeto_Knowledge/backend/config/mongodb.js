9657284e65787bd4e78c3e81c43b4acfed4f2f06
manjor <manoeltavares.jr@gmail.com>
2019-01-23 11:41:49 -0300

    Projeto Final: MongoDB para aplicação heroku

--- projeto_Knowledge/backend/config/mongodb.js
+++ projeto_Knowledge/backend/config/mongodb.js
@@ -1,5 +1,5 @@
 const moongose = require('mongoose')
-moongose.connect(`mongodb://${process.env.MG_HOST}`, { useNewUrlParser: true} )
+moongose.connect(`mongodb://${process.env.MG_USER}:${process.env.MG_PASSWORD}@${process.env.MG_HOST}`, { useNewUrlParser: true} )
     .catch(e => {
         const msg = 'Não foi possível conectar com o MongoDB'
         console.log('\x1b[41m%s\x1b[37m',msg,'\x1b[0m')
