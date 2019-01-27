f54e91ee048b873e142769bc1db3ade4a8f38482
Manoel Tavares <36523015+Manjor@users.noreply.github.com>
2019-01-27 08:18:51 -0300

    Projeto Final: Artigos por Categoria #3

--- projeto_Knowledge/frontend/src/components/article/ArticlesByCategory.vue
+++ projeto_Knowledge/frontend/src/components/article/ArticlesByCategory.vue
@@ -4,7 +4,7 @@
             :main="category.name" sub="Categoria"/>
             <ul>
                 <li v-for="article in articles" :key="article.id">
-                    {{ article.name }}
+                   <ArticleItem :article="article"/>
                 </li>
             </ul>
             <div class="load-more">
@@ -18,10 +18,11 @@
 import { baseApiUrl,showError } from '@/global'
 import axios from 'axios'
 import PageTitle from '../template/PageTitle'
+import ArticleItem from './ArticleItem'
 
 export default {
     name:'ArticlesByCategory',
-    components:{ PageTitle },
+    components:{ PageTitle,ArticleItem },
     data: function(){
         return{
             category: {},
