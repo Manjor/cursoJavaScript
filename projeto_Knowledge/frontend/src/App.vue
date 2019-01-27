65f506626e584a698e1af34ef5ce9084233d8a43
Manoel Tavares <36523015+Manjor@users.noreply.github.com>
2019-01-27 13:20:20 -0300

    Projeto Final: Validar Token #2

--- projeto_Knowledge/frontend/src/App.vue
+++ projeto_Knowledge/frontend/src/App.vue
@@ -3,22 +3,59 @@
 		<Header title="Cod3r - Base de Conhecimento" 
 			:hideToggle="!user"/>
 		<Menu v-if="user"/>
-		<Content/>
+		<Loading v-if="validatingToken" />
+		<Content v-else/>
 		<Footer/>
 	</div>
 </template>
 
 <script>
 /**@ é a referencia do src/ */
+import axios from 'axios'
+import { baseApiUrl, userKey } from '@/global'
 import Header from '@/components/template/Header'
 import Content from '@/components/template/Content'
 import Menu from '@/components/template/Menu'
 import Footer from '@/components/template/Footer'
+import Loading from '@/components/template/Loading'
 import {mapState} from 'vuex'
 export default {
 	name: "App",
-	components: { Header, Menu, Content, Footer },
-	computed: mapState(['isMenuVisible','user'])
+	components: { Header, Menu, Content, Footer, Loading },
+	computed: mapState(['isMenuVisible','user']),
+	data: function(){
+		return {
+			validatingToken: true
+		}
+	},
+	methods:{
+		async validateToken(){
+			this.validatingToken = true
+
+			const json = localStorage.getItem(userKey)
+			const userData = JSON.parse(json)
+			this.$store.commit('setUser', null)
+
+			if(!userData){
+				this.validatingToken = false
+				this.$router.push({ name: 'auth'})
+				return
+			}
+
+			const res = await axios.post(`${baseApiUrl}/validateToken`,userData)
+			
+			if(res.data){
+				this.$store.commit('setUser',userData)
+			}else{
+				localStorage.removeItem(userKey)
+				this.$router.push({ name: 'auth' })
+			}
+			this.validatingToken = false
+		}
+	},
+	created(){
+		this.validateToken()
+	}
 }
 </script>
 
